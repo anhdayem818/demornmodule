9c8935dceb8eb5be788a4f6c180e2ce638f98edf
tnBach <anhdayem818@gmail.com>
2021-11-04 14:40:22 +0700

    CustomModule done

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
 
-import React from 'react';
-import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View, } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, TouchableOpacity, useColorScheme, View, } from 'react-native';
 import CustomModule from './CustomModule';
 
 const App= () => {
-  CustomModule.show()
+  const [androidId, setAndroidId] = useState("123");
+  const getDeviceInfo= async()=>{
+    const deviceInfo = await CustomModule.getDeviceInfo() 
+    setAndroidId(deviceInfo)
+  }
+  // useEffect(() => {
+      
+  //     getDeviceInfo()
+  // }, [])
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
         <Text style={styles.text}> Hello </Text>
+        <TouchableOpacity onPress={getDeviceInfo}>
+          <Text> OnPress</Text>
+        </TouchableOpacity>
+        <Text style={styles.text}> {androidId} </Text>
       </ScrollView>
     </SafeAreaView>
   );
--- android/app/src/main/java/com/demornmodule/CustomModule.java
+++ android/app/src/main/java/com/demornmodule/CustomModule.java
@@ -1,9 +1,11 @@
 package com.demornmodule;
 
+import android.provider.Settings;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
@@ -18,6 +20,16 @@ public class CustomModule extends ReactContextBaseJavaModule {
     @ReactMethod
     public void show(){
         Toast.makeText(reactContext, "Hi Guy.", Toast.LENGTH_SHORT).show();
+    }
+
+    @ReactMethod
+    public void getDeviceInfo(Promise promise){
+        try{
+            String android_id = Settings.Secure.getString(reactContext.getContentResolver(), Settings.Secure.ANDROID_ID);
+            promise.resolve(android_id);
+        }catch (Exception e){
+            promise.reject(e);
+        }
     }
 
     @NonNull
